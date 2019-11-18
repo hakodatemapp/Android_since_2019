afb52526f3d17dd09b62aa3c0de36458935df583
Atria <49768768+Atria64@users.noreply.github.com>
2019-11-18 17:09:13 +0900

    SegmentClusterItem.javaを追加
    クラスタリングを行うためのクラス SegmentClusterItemを追加。
    また、クラスタリングを行うためのユーティリティ、'com.google.maps.android:android-maps-utils:0.5+'を implementation した。

---
+++ app/src/main/java/ac/fun/hakodatemapplus/SegmentClusterItem.java
@@ -0,0 +1,43 @@
+package ac.fun.hakodatemapplus;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.ClusterItem;
+
+///
+//
+// 以下のコードは以下の公式ドキュメントに書いてあるコードをコピペしてちょっとだけ改変したものである
+// https://developers.google.com/maps/documentation/android-sdk/utility/marker-clustering
+//
+///
+
+public class SegmentClusterItem implements ClusterItem {
+    private final LatLng mPosition;
+    private final String mTitle;
+    private final String mSnippet;
+
+    public SegmentClusterItem(double lat, double lng, String title, String snippet) {
+        mPosition = new LatLng(lat, lng);
+        mTitle = title;
+        mSnippet = snippet;
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
+
+    @Override
+    public String getTitle() {
+        return mTitle;
+    }
+
+    @Override
+    public String getSnippet() {
+        return mSnippet;
+    }
+
+}
+
+///
+// コピペおわり
+///
